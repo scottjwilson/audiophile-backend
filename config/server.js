6f54d991214c28361a407b221e5df2755d0ea987
Scott <slimzel@protonmain>
2021-05-24 08:13:51 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6ca10f627aa5456a6615628ae98ae2a3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1fb26db3-bcc6-4114-8893-cf8a114b1a70'
+};
